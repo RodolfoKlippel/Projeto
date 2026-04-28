cc5eb63c4f1b97019a3dc754954313efcd0aee8f
Rodolfo Klippel <dolforr@gmail.com>
2026-04-28 14:01:51 -0300

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import MuObjVet from './MuObjVetor'
+function App() {
+
+  return (
+    <MuObjVet></MuObjVet>
+  );
+}
+
+export default App
